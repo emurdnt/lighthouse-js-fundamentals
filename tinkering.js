eec1b9c1cec18d91a8d778889b453a2013b0a7c9
Em Urdaneta <em.urdaneta94@gmail.com>
2020-07-04 19:14:47 +0000

    created tinkering.js

---
+++ tinkering.js
@@ -0,0 +1,13 @@
+function multiply(a, b) {
+  return a * b;
+}
+
+const result = multiply(2, 4);
+
+console.log(result);
+console.log(true||false);
+console.log(false&&false);
+console.log(!true);
+console.log((13>-7)||(false==0));
+console.log((10==="10")&&(1<=2));
+console.log((3!=6%3)&&!(24>45)&&(!false));
